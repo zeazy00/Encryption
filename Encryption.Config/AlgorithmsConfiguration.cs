3b1cffe261040cb51253c52730d958128e836e0d
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-09-05 20:45:49 +1000

    add: smth like injector

---
+++ Encryption.Config/AlgorithmsConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Encryption.Bll.Vigenere;
+using Encryption.DI;
+
+namespace Encryption.Config
+{
+    public class AlgorithmsConfiguration
+    {
+        private readonly List<IEncryptionAlg> _algorithms;
+
+        public AlgorithmsConfiguration()
+        {
+            _algorithms = new List<IEncryptionAlg>();
+            Setup();
+        }
+
+        protected virtual void Setup()
+        {
+            IAlgorithmConfiguration algorithmConfiguration = GetConfiguration();
+
+            _algorithms.Add(new VigenereAlg(algorithmConfiguration));
+        }
+
+        protected virtual IAlgorithmConfiguration GetConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Encryption.View/ConsoleHelper.cs
+++ Encryption.View/ConsoleHelper.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Encryption.DI;
+
 namespace Encryption.View
 {
     partial class Program
     {
+        IAlgorithmConfiguration SetupAlgorithmConfiguration()
+        {
+            return new 
+        }
     }
 }
