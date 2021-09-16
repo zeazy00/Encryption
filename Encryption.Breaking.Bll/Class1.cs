50318affc500b17f5eeeaa34a9d0dfffbb6cbde4
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2021-09-16 20:35:42 +1000

    add: initial sructure

---
+++ Encryption.Breaking.Bll/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Encryption.Breaking.Bll
+{
+    public class Class1
+    {
+    }
+}
---
+++ Encryption.DI/Break/IEncryptionAlgorithmBreak.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption.DI.Break
+{
+    public interface IEncryptionAlgorithmBreak
+    {
+        string Decipher(string source);
+    }
+}
